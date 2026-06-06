3c28914ee5256fb3c8be0b369da0eeb65fb412a1
ghostkill73 <142703278+ghostkill73@users.noreply.github.com>
2026-06-06 04:54:27 +0000

    update

--- main.go
+++ main.go
@@ -10,52 +10,39 @@ import (
 func main() {
 	list := os.Args[1:]
 
-	resultCh := make(chan string)
-
-	go permuteArgs(resultCh, list)
-
-	for p := range resultCh {
-		fmt.Println(p)
+	if len(list) == 0 {
+		fmt.Printf("Usage: %s <word1> [word2] [word3] ...\n", os.Args[0])
+		os.Exit(1)
 	}
+
+	permuteArgs(list)
 }
 
-func permuteArgs(ch chan string, words []string) {
-	defer close(ch)
-
-	ptrs := make([]*string, len(words))
-
-	for i := range words {
-		ptrs[i] = &words[i]
-	}
-
-	sort.Slice(ptrs, func(i, j int) bool {
-		return *ptrs[i] < *ptrs[j]
-	})
+func permuteArgs(words []string) {
+	sort.Strings(words)
 
 	var buffer bytes.Buffer
 	for {
 		buffer.Reset()
 
-		for _, ptr := range ptrs {
-			buffer.WriteString(*ptr)
+		for _, w := range words {
+			buffer.WriteString(w)
 		}
 
-		ch <- buffer.String()
+		fmt.Println(buffer.String())
 
-		if !nextPermutation(ptrs) {
+		if !nextPermutation(words) {
 			break
 		}
 	}
-
-	return
 }
 
-func nextPermutation(arr []*string) bool {
+func nextPermutation(arr []string) bool {
 	n := len(arr)
 
 	k := -1
 	for i := n - 2; i >= 0; i-- {
-		if *arr[i] < *arr[i+1] {
+		if arr[i] < arr[i+1] {
 			k = i
 			break
 		}
@@ -67,7 +54,7 @@ func nextPermutation(arr []*string) bool {
 
 	l := -1
 	for i := n - 1; i > k; i-- {
-		if *arr[k] < *arr[i] {
+		if arr[k] < arr[i] {
 			l = i
 			break
 		}
